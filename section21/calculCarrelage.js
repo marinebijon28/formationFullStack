5d2e263650becc8169e9c676b43468ed642d0240
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-09-15 11:06:37 +0200

    section 21
    section 21

---
+++ section21/calculCarrelage.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended : false}))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/caculCarrelage.html")
+})
+
+app.post("/", function (req, res) {
+    let aire = req.body.aire
+    let numberCarreaux = (15 * 15) / aire;
+    res.send("Il vous faudra " + numberCarreaux + " pour une surface de " + aire + " m2 !")
+})
+
+
+app.listen(3000, function() {
+    console.log('Exemple app listening on port 3000!')
+})
